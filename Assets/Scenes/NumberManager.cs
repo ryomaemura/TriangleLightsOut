62c3673298e6758dd8717a3849f057d767f405e2
ryomaemura <ryomaemura34@gmail.com>
2022-06-19 18:36:36 +0900

    add triangle-lights-out

--- Assets/Scenes/NumberManager.cs
+++ Assets/Scenes/NumberManager.cs
@@ -6,24 +6,24 @@ using TMPro;
 
 public class NumberManager : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI buttonText1;
-    [SerializeField] TextMeshProUGUI buttonText2;
-    [SerializeField] TextMeshProUGUI buttonText3;
-    [SerializeField] TextMeshProUGUI buttonText4;
-    [SerializeField] TextMeshProUGUI buttonText5;
-    [SerializeField] TextMeshProUGUI buttonText6;
-    [SerializeField] TextMeshProUGUI buttonText7;
-    [SerializeField] TextMeshProUGUI buttonText8;
-    [SerializeField] TextMeshProUGUI buttonText9;
-    [SerializeField] TextMeshProUGUI buttonText10;
-    [SerializeField] TextMeshProUGUI buttonText11;
-    [SerializeField] TextMeshProUGUI buttonText12;
-    [SerializeField] TextMeshProUGUI buttonText13;
-    [SerializeField] TextMeshProUGUI buttonText14;
-    [SerializeField] TextMeshProUGUI buttonText15;
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+    [SerializeField] Button button3;
+    [SerializeField] Button button4;
+    [SerializeField] Button button5;
+    [SerializeField] Button button6;
+    [SerializeField] Button button7;
+    [SerializeField] Button button8;
+    [SerializeField] Button button9;
+    [SerializeField] Button button10;
+    [SerializeField] Button button11;
+    [SerializeField] Button button12;
+    [SerializeField] Button button13;
+    [SerializeField] Button button14;
+    [SerializeField] Button button15;
+    Button[] buttons;
     [SerializeField] TextMeshProUGUI scoreLabel;
-    TextMeshProUGUI[] buttonTexts;
-    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0};
+    int[] numbers = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     int[, ] relasionshipNumbers = {
     //   1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
         {0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -44,17 +44,14 @@ public class NumberManager : MonoBehaviour
         {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
     };
-    int temp = 0;
-    int randomNumber1 = 0;
-    int randomNumber2 = 0;
+    int randomNumber = 0;
     int scoreNumber = 0;
 
     // Start is called before the first frame update
     void Start() {
-        buttonTexts = new TextMeshProUGUI[] {buttonText1, buttonText2, buttonText3, buttonText4, buttonText5, buttonText6, buttonText7, buttonText8, buttonText9, buttonText10, buttonText11, buttonText12, buttonText13, buttonText14, buttonText15};
+        buttons = new Button[] {button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15};
 
-        shuffleNumbers();
-        setNumbers();
+        resetLights();
         scoreReload();
     }
 
@@ -63,39 +60,59 @@ public class NumberManager : MonoBehaviour
     }
 
     public void clickButton(int number) {
+        if (numbers[number - 1] == 1) {
+            buttons[number - 1].GetComponent<Image>().color = new Color32(100, 0, 200, 255);
+            numbers[number - 1] = 0;
+        } else if (numbers[number - 1] == 0) {
+            buttons[number - 1].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            numbers[number - 1] = 1;
+        }
+
+        scoreNumber = scoreNumber + 1;
+        scoreReload();
+
         for (int i = 0; i < numbers.Length; i++) {
-            if (relasionshipNumbers[number - 1, i] == 1 && numbers[i] == 0) {
-                temp = numbers[number - 1];
-                numbers[number - 1] = numbers[i];
-                numbers[i] = temp;
-
-                buttonTexts[number - 1].text = "";
-                buttonTexts[i].text = numbers[i].ToString();
-
-                scoreNumber = scoreNumber + 1;
-                scoreReload();
+            if (relasionshipNumbers[number - 1, i] == 1 && numbers[i] == 1) {
+                buttons[i].GetComponent<Image>().color = new Color32(100, 0, 200, 255);
+                numbers[i] = 0;
+            } else if (relasionshipNumbers[number - 1, i] == 1 && numbers[i] == 0) {
+                buttons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+                numbers[i] = 1;
             }
         }
     }
 
-    public void setNumbers() {
+    public void resetLights() {
         for (int i = 0; i < numbers.Length; i++) {
-            if (numbers[i] != 0) {
-                buttonTexts[i].text = numbers[i].ToString();
-            } else {
-                buttonTexts[i].text = "";
-            }
+            buttons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            numbers[i] = 1;
         }
-    }
 
-    public void shuffleNumbers() {
-        for (int i = 0; i < 30; i++) {
-            randomNumber1 = UnityEngine.Random.Range(0, numbers.Length);
-            randomNumber2 = UnityEngine.Random.Range(0, numbers.Length);
+        for (int i = 0; i < numbers.Length; i++) {
+            // output 0 ~ 1
+            randomNumber = UnityEngine.Random.Range(0, 2);
 
-            temp = numbers[randomNumber1];
-            numbers[randomNumber1] = numbers[randomNumber2];
-            numbers[randomNumber2] = temp;
+            if (randomNumber == 1 && numbers[i] == 1) {
+                buttons[i].GetComponent<Image>().color = new Color32(100, 0, 200, 255);
+                numbers[i] = 0;
+            } else if (randomNumber == 1 && numbers[i] == 0) {
+                buttons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+                numbers[i] = 1;
+            }
+
+            if (randomNumber == 1) {
+                for (int j = 0; j < numbers.Length; j++) {
+                    if (relasionshipNumbers[i, j] == 1) {
+                        if (numbers[j] == 1) {
+                            buttons[j].GetComponent<Image>().color = new Color32(100, 0, 200, 255);
+                            numbers[j] = 0;
+                        } else if (numbers[j] == 0) {
+                            buttons[j].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+                            numbers[j] = 1;
+                        }
+                    }
+                }
+            }
         }
 
         scoreNumber = 0;
